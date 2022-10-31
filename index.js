39e8c0477e7a1aafa7743bde29462868e5ed13bc
TheCodby <thecodby@gmail.com>
2022-11-01 02:18:05 +0300

    update

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+// {name: name, message: message}
+const TEST_MESSAGES = [];
+io.on("connection", (socket) => {
+  console.log(`connect ${socket.id}`);
+  socket.on("messages:fetch", () => {
+    socket.emit("messages:get", { messages: TEST_MESSAGES });
+  });
+  socket.on("messages:send", ({ message }) => {
+    TEST_MESSAGES.push(message);
+    io.emit("messages:response", { message: message });
+  });
+  socket.on("disconnect", () => {
+    console.log("🔥: A user disconnected");
+  });
+});
+httpServer.listen(3001);
